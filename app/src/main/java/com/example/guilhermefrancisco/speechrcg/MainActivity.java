ff7d3e5b4f20af0eeec8836dfcd4f1a0f4bfa2bb
DiskPro <guifreitas89@hotmail.com>
2017-12-31 10:41:45 -0200

    Removed static initialization
    Now it uses a JSON string to map all information regarding images and letters.

--- app/src/main/java/com/example/guilhermefrancisco/speechrcg/MainActivity.java
+++ app/src/main/java/com/example/guilhermefrancisco/speechrcg/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.guilhermefrancisco.speechrcg;
 
 import android.content.ActivityNotFoundException;
+import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.speech.RecognizerIntent;
 import android.support.v7.app.AppCompatActivity;
@@ -14,7 +16,9 @@ import android.widget.TextView;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Locale;
-import java.util.Map;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,61 +26,11 @@ public class MainActivity extends AppCompatActivity {
     private TextView voiceI;
     private ImageButton speakB;
 
-    private String resultTxt = "Hello!";
+    private Context context = this;
 
+    private String resultTxt = "Hello";
 
-    //TODO: Transfer this to a JSON file
-    private static final Map<String, Integer> mapaLetra;
-    static
-    {
-        mapaLetra = new HashMap<String, Integer>();
-        mapaLetra.put("a", 0);
-        mapaLetra.put("b", 1);
-        mapaLetra.put("c", 2);
-        mapaLetra.put("d", 3);
-        mapaLetra.put("e", 4);
-        mapaLetra.put("f", 5);
-        mapaLetra.put("g", 6);
-        mapaLetra.put("h", 7);
-        mapaLetra.put("i", 8);
-        mapaLetra.put("j", 9);
-        mapaLetra.put("k", 10);
-        mapaLetra.put("l", 11);
-        mapaLetra.put("m", 12);
-        mapaLetra.put("n", 13);
-        mapaLetra.put("o", 14);
-        mapaLetra.put("p", 15);
-        mapaLetra.put("q", 16);
-        mapaLetra.put("r", 17);
-        mapaLetra.put("s", 18);
-        mapaLetra.put("t", 19);
-        mapaLetra.put("u", 20);
-        mapaLetra.put("v", 21);
-        mapaLetra.put("w", 22);
-        mapaLetra.put("x", 23);
-        mapaLetra.put("y", 24);
-        mapaLetra.put("z", 25);
-        mapaLetra.put("á", 0);
-        mapaLetra.put("ã", 0);
-        mapaLetra.put("à", 0);
-        mapaLetra.put("é", 4);
-        mapaLetra.put("ó", 14);
-        mapaLetra.put("í", 8);
-        mapaLetra.put("ç", 2);
-        mapaLetra.put("ê", 4);
-        mapaLetra.put("â", 0);
-        mapaLetra.put("0", 26);
-        mapaLetra.put("1", 27);
-        mapaLetra.put("2", 28);
-        mapaLetra.put("3", 29);
-        mapaLetra.put("4", 30);
-        mapaLetra.put("5", 31);
-        mapaLetra.put("6", 32);
-        mapaLetra.put("7", 33);
-        mapaLetra.put("8", 34);
-        mapaLetra.put("9", 35);
-        mapaLetra.put("10", 36);
-    }
+    private HashMap<String, String> MapLetra;
 
     private final Integer imagem_letra[] = {
             R.drawable.a,
@@ -124,6 +78,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        MapLetra = new Gson().fromJson(getString(R.string.map), new TypeToken<HashMap<String, String>>(){}.getType());
+
         voiceI = findViewById(R.id.voiceInput);
         speakB = findViewById(R.id.btnSpeak);
         speakB.setOnClickListener(new View.OnClickListener() {
@@ -135,7 +91,6 @@ public class MainActivity extends AppCompatActivity {
         });
 
         RecyclerView recyclerView = findViewById(R.id.imagens);
-
 
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext(), LinearLayoutManager.HORIZONTAL, false);
         recyclerView.setLayoutManager(layoutManager);
@@ -218,11 +173,12 @@ public class MainActivity extends AppCompatActivity {
 
     private ArrayList<Cell> prepareData(String rsltxt) { //Prepares data and creates list with both images (in LIBRAS) and letters
         ArrayList<Cell> theimage = new ArrayList<>();
+        Resources resources = context.getResources();
 
         String word;
         int newIdx = 0;
         boolean lastword = false;
-        Character LetraSep = ' ';
+        String LetraSep = "";
 
         while (!lastword) {
             if (temEspaco(rsltxt)) {//Checks for existence of space character
@@ -234,11 +190,11 @@ public class MainActivity extends AppCompatActivity {
                 word = rsltxt;
                 lastword = true;
             }
-            if (mapaLetra.get(word) != null) { //Checks for existence of word in the mapaLetra map after string is divided in words
+            if (MapLetra.get(word) != null) { //Checks for existence of word in the mapaLetra map after string is divided in words
                 Cell createList = new Cell();
 
                 createList.setLetra(word);
-                createList.setImg(imagem_letra[mapaLetra.get(word)]);
+                createList.setImg(resources.getIdentifier(MapLetra.get(word), "drawable", context.getPackageName()));
                 theimage.add(createList);
             } else {
                 for (int i = 0; i < word.length(); i++) {
@@ -249,11 +205,13 @@ public class MainActivity extends AppCompatActivity {
                             word.toLowerCase().charAt(i) != '\'' ||
                             word.toLowerCase().charAt(i) != '\"' ||
                             word.toLowerCase().charAt(i) != '!'){
-                        LetraSep = word.toLowerCase().charAt(i);
+                        LetraSep = String.valueOf(word.toLowerCase().charAt(i));
                         Cell createList = new Cell();
 
+                        String test = MapLetra.get(LetraSep);
+
                         createList.setLetra(String.valueOf(LetraSep));
-                        createList.setImg(imagem_letra[mapaLetra.get(String.valueOf(LetraSep))]); //Create object with the image version of the letter and text description
+                        createList.setImg(resources.getIdentifier(MapLetra.get(LetraSep), "drawable", context.getPackageName())); //Create object with the image version of the letter and text description
                         theimage.add(createList); //Add said object to list
                     }
                 }
