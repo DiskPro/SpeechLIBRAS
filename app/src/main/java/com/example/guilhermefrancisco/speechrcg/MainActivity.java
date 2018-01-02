0f42209f360b87701a2d32f7a24e0973c119b9f4
DiskPro <guifreitas89@hotmail.com>
2018-01-02 09:16:09 -0200

    Removed superfluous objects

--- app/src/main/java/com/example/guilhermefrancisco/speechrcg/MainActivity.java
+++ app/src/main/java/com/example/guilhermefrancisco/speechrcg/MainActivity.java
@@ -32,46 +32,6 @@ public class MainActivity extends AppCompatActivity {
 
     private HashMap<String, String> MapLetra;
 
-    private final Integer imagem_letra[] = {
-            R.drawable.a,
-            R.drawable.b,
-            R.drawable.c,
-            R.drawable.d,
-            R.drawable.e,
-            R.drawable.f,
-            R.drawable.g,
-            R.drawable.h,
-            R.drawable.i,
-            R.drawable.j,
-            R.drawable.k,
-            R.drawable.l,
-            R.drawable.m,
-            R.drawable.n,
-            R.drawable.o,
-            R.drawable.p,
-            R.drawable.q,
-            R.drawable.r,
-            R.drawable.s,
-            R.drawable.t,
-            R.drawable.u,
-            R.drawable.v,
-            R.drawable.w,
-            R.drawable.x,
-            R.drawable.y,
-            R.drawable.z,
-            R.drawable.zero,
-            R.drawable.um,
-            R.drawable.dois,
-            R.drawable.tres,
-            R.drawable.quatro,
-            R.drawable.cinco,
-            R.drawable.seis,
-            R.drawable.sete,
-            R.drawable.oito,
-            R.drawable.nove,
-            R.drawable.dez
-    };
-
     @Override
 
     protected void onCreate(Bundle savedInstanceState) {
